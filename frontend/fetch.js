a1af8d795b4fc558d100e0be96e052a7f4867651
yRagax <01168917@pw.edu.pl>
2023-07-16 00:32:57 +0200

    add frontend to explore search button

--- frontend/fetch.js
+++ frontend/fetch.js
@@ -1,14 +1,24 @@
-fetch(`http://localhost:8080/exercises`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
+requestExercises('');
+
+document.querySelector('input')
+    .addEventListener('input', event => {
+        requestExercises(event.target.value);
+        console.log(event.target.value);
+    });
+
+function requestExercises(filter) {
+    const url = filter ? `http://localhost:8080/exercises/${filter}` : `http://localhost:8080/exercises`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
     })
-    .then(response => response.json())
-    .then(data => {
-        let insideGridHTML = '';
-        data.forEach(exercise => {
-            insideGridHTML += `
+        .then(response => response.json())
+        .then(data => {
+            let insideGridHTML = '';
+            data.forEach(exercise => {
+                insideGridHTML += `
                 <div class="exercise-item">
                     <img src="images/${exercise.name.toLowerCase().replace(/ /g, '_')}.jpg">
                     <div class="exercise-text">
@@ -17,19 +27,8 @@ fetch(`http://localhost:8080/exercises`, {
                         <p class="exercise-description">Type: ${exercise.type.charAt(0).toUpperCase()+exercise.type.slice(1)}</p>
                     </div>
                 </div>`;
-        });
-        document.querySelector('.exercise-grid').innerHTML = insideGridHTML;
-    })
-    .catch(error => console.error('Error:', error));
-
-fetch(`http://localhost:8080/exercises/be`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => console.error('Error:', error));
+            });
+            document.querySelector('.exercise-grid').innerHTML = insideGridHTML;
+        })
+        .catch(error => console.error('Error:', error));
+}
