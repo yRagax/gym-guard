7150e20bd10b96359afbe8edc94dbbcac8f6dc5e
yRagax <01168917@pw.edu.pl>
2023-07-30 17:28:47 +0200

    add workout started coloring

--- frontend/scripts/general.js
+++ frontend/scripts/general.js
@@ -5,3 +5,8 @@ if(localStorage.getItem('isWorkoutStarted') === null) {
 if(localStorage.getItem('workoutIndex') === null) {
     localStorage.setItem('workoutIndex', -1);
 }
+
+if(localStorage.getItem('isWorkoutStarted') === 'true' &&
+    !window.location.href.endsWith('exercise.html')) {
+    document.querySelector('header').children[0].setAttribute('style', 'background-color: lightblue;');
+}
